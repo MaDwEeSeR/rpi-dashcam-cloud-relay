9b76fb47407c2e398afbcafbb424b6c24ac95e56
Sebastian Jørgensen <sebjorg@gmail.com>
2023-09-01 04:53:41 +0200

    bugfix install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ DIR=$(dirname "$0")
 npm install
 npm run build
 
-systemctl link /${DIR}/systemd/rpi-dashcam-cloud-relay.service
-systemctl enable /opt/${DIR}/systemd/rpi-dashcam-cloud-relay.service
+systemctl link ${DIR}/systemd/rpi-dashcam-cloud-relay.service
+systemctl enable ${DIR}/systemd/rpi-dashcam-cloud-relay.service
